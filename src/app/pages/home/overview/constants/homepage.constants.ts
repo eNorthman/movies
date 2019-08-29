a503a59c2d58afb519c31c16cd4783201b3030c1
eric van der helm <eric.vanderhelm@kpn.com>
2019-08-29 15:12:12 +0200

    feat: updated constants

--- src/app/pages/home/overview/constants/homepage.constants.ts
+++ src/app/pages/home/overview/constants/homepage.constants.ts
@@ -3,6 +3,6 @@ import { environment } from 'src/environments/environment';
 const apiUrl = 'https://www.omdbapi.com/';
 
 export const API_CONSTANTS = {
-  getMovieBySearch: (title: string, type: string = 'movie'): string => `${apiUrl}?apikey=${environment.apiKey}&s=${title}&page=1&${type}`,
+  getMovieBySearch: (title: string, type: string = 'movie'): string => `${apiUrl}?apikey=${environment.apiKey}&s=${title}&page=1&type=${type}`,
   getMovieDetails: (title: string = 'Game of Thrones'): string => `${apiUrl}?apikey=${environment.apiKey}&t=${title}`,
 };
